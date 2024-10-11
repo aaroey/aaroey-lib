28edea414525267594bd6971c1fdda73ee748cab
Guangda Lai <31743510+aaroey@users.noreply.github.com>
2024-10-11 13:03:04 -0700

    support computing all hashes in one go

--- image/create_dup_images_html.py
+++ image/create_dup_images_html.py
@@ -18,13 +18,12 @@ import utils
 @typing.runtime_checkable
 class Deduper(typing.Protocol):
 
-  @property
   @abc.abstractmethod
-  def name(self) -> str:
+  def hash_types(self) -> tuple[str, ...]:
     ...
 
   @abc.abstractmethod
-  def compute_hash(self, file_path) -> str | None:
+  def compute_hashes(self, file_path: str, file_bytes: bytes) -> dict[str, str]:
     ...
 
   @abc.abstractmethod
@@ -32,86 +31,112 @@ class Deduper(typing.Protocol):
     ...
 
 
-def compute_md5(file_path):
+def compute_md5(file_bytes: bytes):
   """Calculates the MD5 hash of a file."""
   hash_md5 = hashlib.md5()
-  with open(file_path, 'rb') as f:
-    for chunk in iter(lambda: f.read(4096), b''):
-      hash_md5.update(chunk)
+  hash_md5.update(file_bytes)
   return hash_md5.hexdigest()
 
 
-class DeduperMd5:
-  name: str = 'md5'
+class DeduperMd5(Deduper):
 
-  def compute_hash(self, file_path):
+  def hash_types(self):
+    return ('md5',)
+
+  def compute_hashes(self, file_path: str, file_bytes: bytes):
+    hashes = {}
     try:
-      md5 = compute_md5(file_path)
+      md5 = compute_md5(file_bytes)
+      hashes['md5'] = md5
     except Exception as e:
       print(f'\033[91m=> Failed to compute md5 of {file_path}: {e}\033[0m')
-      return None
+    return hashes
 
   def key_fn(self, file: utils.ImageFileMeta):
     return file.relative_path
 
 
-@dataclasses.dataclass(kw_only=True)
-class DeduperSimhash:
-  thumbnail_size: int = 8
-  gray_scale: int = 4
-  _gray_scale_bits: int = dataclasses.field(init=False)
-
-  def __post_init__(self):
-    assert self.thumbnail_size >= 4 and self.thumbnail_size <= 8, f'{self.thumbnail_size=}'
-    assert self.gray_scale in (2, 4), f'{self.gray_scale=}'
-    self._gray_scale_bits = (2 if self.gray_scale == 4 else 1)
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class SimhashConfig:
+  tsize: int  # Thumbnail size.
+  gscale_bits: int
 
   @property
   def name(self) -> str:
-    size = self.thumbnail_size**2
-    return f'simhash{size}x{self.gray_scale}'
+    size = self.tsize**2
+    return f'simhash{size}x{self.gscale}'
 
-  def compute_hash(self, file_path):
+  @property
+  def gscale(self) -> int:
+    return 2**self.gscale_bits
+
+
+class DeduperSimhash:
+
+  def __init__(self):
+    # yapf: disable
+    self.cfgs = [
+        SimhashConfig(tsize=tsize, gscale_bits=gscale_bits)
+        for tsize, gscale_bits in (
+            (4, 2), (5, 2), (6, 2), (7, 2), (8, 2), # 4 grayscale
+            (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), # 2 grayscale
+        )
+    ]
+    # yapf: enable
+
+  def hash_types(self):
+    return tuple(cfg.name for cfg in self.cfgs)
+
+  def compute_hashes(self, file_path: str, file_bytes: bytes):
+    hashes = {}
     try:
-      img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-      if img is None:
+      # img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+      nparr = np.frombuffer(file_bytes, np.uint8)
+      orimg = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+      if orimg is None:
         raise ValueError(f"Could not read image from: {file_path}")
-      img = cv2.resize(img, (self.thumbnail_size, self.thumbnail_size))
 
-      # Convert to 4-level grayscale (0, 64, 128, 192)
-      # img = img // 64
-      m1 = np.min(img)
-      m2 = np.max(img)
-      img = (img - m1) // ((m2 - m1) // self.gray_scale)
+      for cfg in self.cfgs:
+        img = cv2.resize(orimg, (cfg.tsize, cfg.tsize))
 
-      # Convert the NxN matrix to a binary string. E.g. 8x8 with 4 grayscale
-      # will be converted to 128bit binary string.
-      binary_string = ''.join(format(i, '02b') for i in img.flatten())
-      integer_representation = int(binary_string, self._gray_scale_bits)
-      return hex(integer_representation)
+        # Convert to 4-level grayscale (0, 64, 128, 192)
+        # img = img // 64
+        m1 = np.min(img)
+        m2 = np.max(img)
+        img = (img - m1) // ((m2 - m1) // cfg.gscale)
+
+        # Convert the NxN matrix to a binary string. E.g. 8x8 with 4 grayscale
+        # will be converted to 128bit binary string.
+        binary_string = ''.join(
+            format(i, f'0{cfg.gscale_bits}b') for i in img.flatten()
+        )
+        integer_representation = int(binary_string, 2)
+        hashes[cfg.name] = hex(integer_representation)
     except Exception as e:
       print(f'\033[91m=> Failed to simhash {file_path}: {e}\033[0m')
-      return None
+    return hashes
 
   def key_fn(self, file: utils.ImageFileMeta):
     return -file.w, -file.h, -file.size, file.relative_path
 
 
 def maybe_move(
-    hash_to_files: dict[str, utils.ImageFileMeta], deduper: Deduper,
-    src_root_dir: str, dst_root_dir: str | None
+    hash_to_metas: dict[str, list[utils.ImageFileMeta]],
+    deduper: Deduper,
+    src_root_dir: str,
+    dst_root_dir: str | None,
 ):
-  for files in hash_to_files.values():
+  for files in hash_to_metas.values():
     files.sort(key=deduper.key_fn)
 
   if not dst_root_dir:
-    return hash_to_files
+    return hash_to_metas
 
   if not os.path.exists(dst_root_dir):
     os.makedirs(dst_root_dir)
 
-  updated_hash_to_files = {}
-  for key, files in hash_to_files.items():
+  updated_hash_to_metas = {}
+  for key, files in hash_to_metas.items():
     moved_files = [files[0]]  # This is the best file, don't move it.
     for file in files[1:]:
       src_path = os.path.join(src_root_dir, file.relative_path)
@@ -122,36 +147,20 @@ def maybe_move(
       utils.move(src_path, dst_path)
       moved_files.append(dataclasses.replace(file, relative_path=dst_path))
 
-    updated_hash_to_files[key] = moved_files
-  return updated_hash_to_files
+    updated_hash_to_metas[key] = moved_files
+  return updated_hash_to_metas
 
 
-def dedup_files(src_root_dir: str, dst_root_dir: str | None, mode: str):
-  """Walks through the directory, computes MD5s, and generates the HTML."""
-  if mode == 'md5':
-    deduper = DeduperMd5()
-  elif mode == 'sim64x4':
-    deduper = DeduperSimhash(thumbnail_size=8, gray_scale=4)
-  elif mode == 'sim49x4':
-    deduper = DeduperSimhash(thumbnail_size=7, gray_scale=4)
-  elif mode == 'sim36x4':
-    deduper = DeduperSimhash(thumbnail_size=6, gray_scale=4)
-  elif mode == 'sim25x4':
-    deduper = DeduperSimhash(thumbnail_size=5, gray_scale=4)
-  elif mode == 'sim16x4':
-    deduper = DeduperSimhash(thumbnail_size=4, gray_scale=4)
-  else:
-    raise ValueError(f'Invalid mode: {mode}')
+def compute_hashes(
+    src_root_dir: str, hash_type_to_deduper: dict[str, Deduper],
+    file_key_to_hash_loaded: dict[str, dict[str, str]]
+):
+  """Compute the hash values for all known hash types."""
+  # Maps hash_type to {(file_path, file_size): hash_value}
+  file_key_to_hash_new = collections.defaultdict(dict)
+  # Maps hash_type to {hash_value: list[ImageFileMeta]}
+  hash_to_metas = collections.defaultdict(lambda: collections.defaultdict(list))
 
-  hash_json = os.path.join(src_root_dir, f'hash_{deduper.name}.json')
-  file_to_hash_loaded = dict()  # Maps (path, size) to hash str
-  if os.path.exists(hash_json):
-    with open(hash_json, 'r') as f:
-      file_to_hash_loaded = json.load(f)
-
-  # Compute the hash values.
-  file_to_hash_new = dict()  # Maps (str, int) to hash str
-  hash_to_files = collections.defaultdict(list)
   num_processed = 0
   for cur_path, _, files in os.walk(src_root_dir):
     for filename in files:
@@ -168,12 +177,16 @@ def dedup_files(src_root_dir: str, dst_root_dir: str | None, mode: str):
       # hash file after an image is removed and a new image is added with the
       # same name.
       key = f'{fullpath.encode("utf-8")}:{size}'
-      if key in file_to_hash_loaded:
-        hash_value = file_to_hash_loaded[key]
-      else:
-        hash_value = deduper.compute_hash(fullpath)
+      # Get from loaded or {}.
+      hash_values = file_key_to_hash_loaded.get(key, {})
+      file_bytes = open(fullpath, 'rb').read()
+      for hash_type, deduper in hash_type_to_deduper.items():
+        if hash_type not in hash_values:
+          hash_values.update(deduper.compute_hashes(fullpath, file_bytes))
+      file_key_to_hash_new[key] = hash_values
 
-      if hash_value is None:  # Skip if failed to get the hash, maybe non image.
+      if len(hash_values) != len(hash_type_to_deduper):
+        # Skip if failed to get all hashes, maybe non image.
         continue
 
       try:
@@ -183,41 +196,73 @@ def dedup_files(src_root_dir: str, dst_root_dir: str | None, mode: str):
         print(f'\033[91m=> Failed to get image size for {fullpath}\033[0m')
         continue
 
-      file_to_hash_new[key] = hash_value
       relative_path = os.path.relpath(fullpath, src_root_dir)
-      hash_to_files[hash_value].append(
-          utils.ImageFileMeta(
-              relative_path=relative_path, size=size, w=width, h=height
-          )
-      )
+      for hash_type, hash_val in hash_values.items():
+        hash_to_metas[hash_type][hash_val].append(
+            utils.ImageFileMeta(
+                relative_path=relative_path, size=size, w=width, h=height
+            )
+        )
+  return file_key_to_hash_new, hash_to_metas
 
-  # Move the duplicates with largest filename to dst_root_dir.
-  updated_hash_to_files = maybe_move(
-      hash_to_files, deduper, src_root_dir, dst_root_dir
+
+def dedup_files(
+    src_root_dir: str,
+    dst_root_dir: str | None,
+    hash_type_to_move: str | None = None
+):
+  """Walks through the directory, computes MD5s, and generates the HTML."""
+  all_dedupers = (DeduperMd5(), DeduperSimhash())
+  hash_type_to_deduper = {}
+  for deduper in all_dedupers:
+    for hash_type in deduper.hash_types():
+      assert hash_type not in hash_type_to_deduper, f'{hash_type} already exists.'
+      hash_type_to_deduper[hash_type] = deduper
+
+  file_json_hash = os.path.join(src_root_dir, f'hash_all.json')
+  # Maps (file_path, file_size) to {hash_type: hash_value} dict.
+  file_key_to_hash_loaded = collections.defaultdict(dict)
+  if os.path.exists(file_json_hash):
+    with open(file_json_hash, 'r') as f:
+      file_key_to_hash_loaded = json.load(f)
+
+  # Computes the hashes.
+  file_key_to_hash_new, hash_to_metas = compute_hashes(
+      src_root_dir, hash_type_to_deduper, file_key_to_hash_loaded
   )
 
-  # Generate the html for comparison.
-  html = utils.generate_html(updated_hash_to_files, scale_image_by_width=True)
-  html_file = os.path.join(src_root_dir, f'image_mapping_{deduper.name}.html')
-  with open(html_file, 'w') as f:
-    f.write(html)
-
   # Save the hash to avoid recomputation next time.
-  with open(hash_json, 'w') as f:
-    json.dump(file_to_hash_new, f, indent=2)
+  with open(file_json_hash, 'w') as f:
+    json.dump(file_key_to_hash_new, f, indent=2)
+
+  # Move the duplicates with largest filename to dst_root_dir.
+  if hash_type_to_move is not None:
+    updated_hash_to_metas = maybe_move(
+        hash_to_metas[hash_type_to_move],
+        hash_type_to_deduper[hash_type_to_move], src_root_dir, dst_root_dir
+    )
+    hash_to_metas[hash_type_to_move] = updated_hash_to_metas
+
+  # Generate the htmls for comparison.
+  for hash_type, deduper in hash_type_to_deduper.items():
+    html = utils.generate_html(
+        hash_to_metas[hash_type], scale_image_by_width=True
+    )
+    html_file = os.path.join(src_root_dir, f'image_mapping_{hash_type}.html')
+    with open(html_file, 'w') as f:
+      f.write(html)
 
 
 if __name__ == '__main__':
-  # src_root_dir = '/Users/laigd/Documents/images/eee/selected-未筛选'
   src_root_dir = '/Users/laigd/Documents/images/eee/网页'
-  # src_root_dir = '/tmp/md5test'
 
   dst_root_dir = '/Users/laigd/.Trash/2'
   dst_root_dir = None
 
-  mode = 'md5'
-  mode = 'sim64x4'  # Verified.
-  mode = 'sim49x4'  # Verified, 1/200 dups are similar images (error).
-  mode = 'sim36x4'  # TBD
+  hash_type_to_move = None
 
-  dedup_files(src_root_dir=src_root_dir, dst_root_dir=dst_root_dir, mode=mode)
+  dedup_files(
+      src_root_dir=src_root_dir,
+      dst_root_dir=dst_root_dir,
+      hash_type_to_move=hash_type_to_move
+  )
